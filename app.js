c8fa9d39e41d38dc5ad90f34a6bf48681307d1a3
Grant <gbentley0618@comcast.net>
2023-08-05 13:49:35 -0500

    Elements created from API and on index.HTML

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const post =  fetch('https://jsonplaceholder.typicode.com/todos/1') 
 .then((textData) => textData.json())
-.then((postData) => {
-    const creatPElement = document.createElement('p');
-    //creatPElement.appendChild(document.createTextNode(postData));
-    creatPElement.insertAdjacetText(postData) 
-        function insertElement() {
-            const element = document.querySelector('div:first-child');
-            element.insertAdjacentElement('afterend', creatPElement);
-        };
-    insertElement();
+.then((postData) => { const data = {...postData};
+const mainDiv = document.getElementById("parentDiv");
+
+    const creatPElement1 = document.createElement('p');
+    const elementData1 = document.createTextNode(`userId: ${data.userId}`);
+    creatPElement1.appendChild(elementData1); 
+    mainDiv.appendChild(creatPElement1);
+    
+    const creatPElement2 = document.createElement('p');
+    const elementData2 = document.createTextNode(`id: ${data.id}`);
+    creatPElement2.appendChild(elementData2); 
+    mainDiv.appendChild(creatPElement2);
+    
+    const creatPElement3 = document.createElement('p');
+    const elementData3 = document.createTextNode(`title: ${data.title}`);
+    creatPElement3.appendChild(elementData3); 
+    mainDiv.appendChild(creatPElement3);
+
+    const creatPElement4 = document.createElement('p');
+    const elementData4 = document.createTextNode(`completed: ${data.completed}`);
+    creatPElement4.appendChild(elementData4); 
+    mainDiv.appendChild(creatPElement4);
 
 });
-    //.forEach
-
